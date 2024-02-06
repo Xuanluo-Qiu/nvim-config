7881f2dbf04db440303919951748d2da26c062dd
Xuanluo-Qiu <1307774733@qq.com>
2024-02-06 10:44:20 +0800

    update!

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -47,7 +47,7 @@ return {
     dashboard.section.header.opts.hl = "AlphaHeader"
     dashboard.section.buttons.opts.hl = "AlphaButtons"
     dashboard.section.footer.opts.hl = "AlphaFooter"
-    dashboard.opts.layout[1].val = 5
+    dashboard.opts.layout[1].val = 2
     return dashboard
   end,
   config = function(_, dashboard)
